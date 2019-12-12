cdee11124d693b134080ae5cf9cb7f9b5f686678
likeke <740539033@qq.com>
2019-12-12 10:01:55 +0000

    管理员可删除用户

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -18,6 +18,7 @@ class UsersTableSeeder extends Seeder
         $user = User::find(1);
         $user->name = 'likeke';
         $user->email = '740539033@qq.com';
+        $user->is_admin = true;
         $user->save();
     }
 }
