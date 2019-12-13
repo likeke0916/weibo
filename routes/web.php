a902c9045ae7ff3de586e9edb1be2253f426623d
likeke <740539033@qq.com>
2019-12-13 07:33:02 +0000

    重置密码

--- routes/web.php
+++ routes/web.php
@@ -19,9 +19,16 @@ Route::get('/about', 'StaticPagesController@about')->name('about');
 Route::get('signup', 'UsersController@create')->name('signup');
 
 Route::resource('users', 'UsersController');
-
+//登录
 Route::get('login', 'SessionsController@create')->name('login');
 Route::post('login', 'SessionsController@store')->name('login');
 Route::delete('logout', 'SessionsController@destroy')->name('logout');
 
+
 Route::get('sigup/confirm/{token}', 'UsersController@confirmEmail')->name('confirm_email');
+
+//重置密码
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
